58ad43ea15331aae839c0f5b5718aa7bca43b4ae
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-05-03 10:29:05 -0400

    Basic css and functionality - add button, click button -> AJAX call -> display GIFs, radio button to change quanity

--- README.md
+++ README.md
@@ -4,46 +4,36 @@ Week 06 - Homework: Use the GIPHY API to dynamically create web pages based on u
 ## My Portfolio 
 
 See how it works for yourself:
-
-link1
-
-link2 my portfolio
+link
+*Future* link my portfolio
 
 ## Objectives
 
 As with every week, leverage knowledge from the previous weeks, add the following: 
-
 1. APIs & AJAX - first homework assignment using these whihc will be critical to the upcoming three projects
-
-2. First homewrok requiring the use of forms to get user input to get a topic for gifs they are intrested in seeing
-
+2. First homework requiring the use of forms to get user input to get a topic for gifs
 3. More practice dynmaically creating html elements 
 
 ## Assignment
 
 1. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-
 2. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
 3. Under every gif, display its rating (PG, G, so on).
    * This data is provided by the GIPHY API.
    * Only once you get images displaying with button presses should you move on to the next step.
-
 4. Add a form to your page takes the value from a user input box and adds it into your `topics` array. Then make a function call that takes each topic in the array remakes the buttons on the page.
 
 ### Bonus Goals
 
 1. Ensure your app is fully mobile responsive.
-
+    - Using flexbox, to be done
 2. Allow users to request additional gifs to be added to the page.
-   * Each request should ADD 10 gifs to the page, NOT overwrite the existing gifs.
-
+    * Each request should ADD 10 gifs to the page, NOT overwrite the existing gifs.
+    - Completed, only way to 
 3. List additional metadata (title, tags, etc) for each gif in a clean and readable format.
-
 4. Include a 1-click download button for each gif, this should work across device types.
-
 5. Integrate this search with additional APIs such as OMDB, or Bands in Town. Be creative and build something you are proud to showcase in your portfolio
-
+    - use merriam-webster dictionary api
 6. Allow users to add their favorite gifs to a `favorites` section.
    * This should persist even when they select or add a new topic.
    * If you are looking for a major challenge, look into making this section persist even when the page is reloaded(via localStorage or cookies).
@@ -52,7 +42,10 @@ As with every week, leverage knowledge from the previous weeks, add the followin
 
 Things that I'm going to add to challange myself.
 
-1. 
+1. Use radio buttons to change the number of GIFs that are displayed
+2. Use _offset_ to load different images
+3. Use local storage to save array of topics added, so refresh will delete GIFS
+n. Error handling
 
 ## Necessary Evil
 - GIPHY API developer's key can expire or get overloaded 
--- assets/js/giftastic.js
+++ assets/js/giftastic.js
@@ -1,78 +1,79 @@
-      // array of topics with default values 
-      var topics = ["dogs", "hippos", "zebras"];
+// ///////// Variables 
+var topics = ["dog", "hippopotamus", "zebra"];
+var searchQuanity = 10;
 
-      // displayMovieInfo function re-renders the HTML to display the appropriate content
-      function displayGif() {
+// ///////// Functions
 
-        var gifName = $(this).attr("data-name");
-        var topicContainer = $("#gif-display");
-        topicContainer.addClass("gif-item-container");
-        var topicTitle = $("<div>");
-        topicTitle.addClass("gif-item-title");
-        topicTitle.text(gifName);
-        
+// displayGif function re-renders the HTML to display the appropriate content
+function displayGif() {
 
-        // var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=JfHIP0y01p8J0dSWEMChmhN2aWoTT7j6&tag&tag=" + gifName;
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JfHIP0y01p8J0dSWEMChmhN2aWoTT7j6&limit=10&q=" + gifName;
-        // Creating an AJAX call for the specific movie button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-            console.log(response);
-            for (var i=0; i<response.data.length; i++) {
-                    var imageUrl = response.data[i].images.fixed_width.url;
-                    console.log (i + " : " + imageUrl);
- 
-                var topicImage = $("<img>");
+    var gifName = $(this).attr("data-name");
+    var topicContainer = $("#gif-display");
+    topicContainer.addClass("gif-item-container");
+    var topicTitle = $("<div>");
+    topicTitle.addClass("gif-item-title");
+    topicTitle.text(gifName);
 
-                topicImage.attr("src", imageUrl);
-                topicImage.attr("alt", gifName + " image")
-                topicImage.addClass("gif-item");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JfHIP0y01p8J0dSWEMChmhN2aWoTT7j6&limit=" + searchQuanity + "&q=" + gifName;
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        // console.log(response);
+        for (var i=0; i<response.data.length; i++) {
 
-                topicContainer.prepend(topicImage);
-            }
-            topicContainer.prepend(topicTitle);
-        });
-        
-      }
+            var imageUrl = response.data[i].images.fixed_height.url;
+            var topicImage = $("<img>");
+            topicImage.attr("src", imageUrl);
+            topicImage.attr("alt", gifName + " image")
+            topicImage.addClass("gif-item");
 
-      
-      function renderTopicButtons() {
-
-        $("#buttons-display").empty();
-        $("#buttons-display").addClass("topic-buttons");
-        
-        for (var i = 0; i < topics.length; i++) {
-          var topicBtn = $("<button>");
-          topicBtn.addClass("topic-button");
-          topicBtn.attr("data-name", topics[i]);
-          topicBtn.text(topics[i]);
-          $("#buttons-display").append(topicBtn);
+            topicContainer.prepend(topicImage);
         }
-      }
-
+        topicContainer.prepend(topicTitle);
+    }); //error handling?
+}
       
-      $("#add-topic-button").on("click", function(event) {
-        event.preventDefault();
-        
-        var topic = $("#gif-input").val().trim();
+function renderTopicButtons() {
 
-        $("#gif-input").val();
+    $("#buttons-display").empty();
+    $("#buttons-display").addClass("topic-buttons");
 
-        // Adding movie from the textbox to our array
-        topics.push(topic);
+    for (var i = 0; i < topics.length; i++) {
+      var topicBtn = $("<button>");
+      topicBtn.addClass("topic-button");
+      topicBtn.attr("data-name", topics[i]);
+      topicBtn.text(topics[i]);
+      $("#buttons-display").append(topicBtn);
+    }
+}
 
-        // Calling renderButtons which handles the processing of our movie array
-        renderTopicButtons();
-      });
+  // /////////////////// LISTENERS
 
+  $("#add-topic-button").on("click", function(event) {
+      event.preventDefault();
+      
+      var topic = $("#gif-input").val().trim();
+      topics.push(topic);
 
-      // /////////////////// LISTENERS
-      // Adding a click event listener to all elements with a class of "movie-btn"
-      $(document).on("click", ".topic-button", displayGif);
+      // Add display favorites button (here or in render)
 
-      // Calling the renderButtons function to display the intial buttons
       renderTopicButtons();
 
-    
+      $("#gif-input").val("");
+  });
+
+  $(document).on("click", ".topic-button", displayGif);
+
+  // $(document).on("click", ".favs-button", displayFav);
+
+  // $(document).on("click", ".add-favorites-button", addFav);
+
+  $('input[type="radio"]').on('change', function(e) { 
+      searchQuanity = $(this).val();
+  });
+
+  // ////////////////// Initialization
+  renderTopicButtons();
+
