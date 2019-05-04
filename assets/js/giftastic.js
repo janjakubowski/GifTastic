4cec4c559376f9ed0be90999445d9e89e155a958
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-05-04 00:45:36 -0400

    toogle favorites working, topic buttons saved to sessionStorage

--- assets/js/giftastic.js
+++ assets/js/giftastic.js
@@ -1,5 +1,5 @@
 // ///////// Variables 
-var topics = ["dog", "hippopotamus", "zebra"];
+var initialTopics = ["dog", "hippopotamus", "zebra"];
 // const originalNumTopics = topics.length;
 var searchQuanity = 10;
 
@@ -10,65 +10,91 @@ var searchQuanity = 10;
 
 function toggleFavorite () {
     // var gifUid = $(this).attr("data-uid");
-    console.log("favorite clicked");
+    // console.log("favorite clicked");
+    var uid = $(this).attr("data-uid");
+    var isFav = $(this).attr("data-isfav");
+    console.log(uid + isFav)
+    if (isFav == "Y") {
+        $(this).removeClass("is-favorite");
+        $(this).attr("data-isfav", "N");
+    } else {
+        $(this).addClass("is-favorite");
+        $(this).attr("data-isfav", "Y");
+    }
+}
 
+function addButton () {
+
+    event.preventDefault();
+      
+    var topic = $("#gif-input").val().trim();
+    topics.push(topic);
+    displayTopicButtons(topics);
+    sessionStorage.setItem("topics", JSON.stringify(topics));
+    $("#gif-input").val("");
 }
 
 function removeButton () {
 
     var removeMe = $(this).attr("data-name");
     topics.splice(topics.indexOf(removeMe),1);
-    displayTopicButtons();
-
+    displayTopicButtons(topics);
+    sessionStorage.setItem("topics", JSON.stringify(topics));
 }
 
 function displayGif() {
 
-    var gifName = $(this).attr("data-name");
-    var topicContainer = $("#gif-display");
-    topicContainer.addClass("gif-item-container");
-    var topicTitle = $("<div>");
-    topicTitle.addClass("gif-item-title");
-    topicTitle.text(gifName);
+    var topicName = $(this).attr("data-name");
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JfHIP0y01p8J0dSWEMChmhN2aWoTT7j6&limit=" + searchQuanity + "&q=" + gifName;
+    var allTopicContainer = $("#gif-display");
+        // topicContainer.addClass("gif-item-container");
+    var topicContainer = $("<div>");
+        topicContainer.addClass("gif-one-topic-container");
+
+    var topicTitle = $("<div>");
+        topicTitle.addClass("gif-item-title");
+        topicTitle.text(topicName);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JfHIP0y01p8J0dSWEMChmhN2aWoTT7j6&limit=" + searchQuanity + "&q=" + topicName;
     
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        // console.log(response);
+        
         for (var i=0; i<response.data.length; i++) {
 
-            // var imageUrl = response.data[i].images.fixed_height.url;
+            var imageUrl = response.data[i].images.fixed_height.url;
+            var rating = response.data[i].rating;
             
             var topicItem = $("<div>");
-            topicItem.addClass("gif-item");
-
-            var imageUrl = response.data[i].images.fixed_height.url;
-            var topicImage = $("<img>");
-            topicImage.attr("src", imageUrl);
-            topicImage.attr("alt", gifName + " image");
-            topicImage.addClass("gif-image");
+                topicItem.addClass("gif-item");
             
-            var rating = response.data[i].rating;
+            var topicImage = $("<img>");
+                topicImage.attr("src", imageUrl);
+                topicImage.attr("alt", topicName + " image");
+                topicImage.addClass("gif-image");
+            
             var topicRating = $("<p>");
-            topicRating.text(rating);
-            topicRating.addClass("gif-rating");
+                topicRating.text(rating);
+                topicRating.addClass("gif-rating");
 
             var topicFavorite = $("<p>");
-            topicFavorite.html('<i class="fa fa-heart" aria-hidden="true"></i>');
-            topicFavorite.addClass("gif-favorite");
+                topicFavorite.html('<i class="fa fa-heart" aria-hidden="true"></i>');
+                topicFavorite.addClass("gif-favorite");
+                topicFavorite.attr("data-uid", response.data[i].id);
+                topicFavorite.attr("data-isfav", "N");
 
             topicItem.append(topicImage, topicRating, topicFavorite);
 
             topicContainer.prepend(topicItem);
         }
         topicContainer.prepend(topicTitle);
+        allTopicContainer.prepend(topicContainer);
     }); //error handling?
 }
       
-function displayTopicButtons() {
+function displayTopicButtons(topics) {
 
     $("#buttons-display").empty();
     $("#buttons-display").addClass("topic-buttons");
@@ -80,13 +106,7 @@ function displayTopicButtons() {
             topicBtn.addClass("topic-button");
             topicBtn.attr("data-name", topics[i]);
             topicBtn.text(topics[i]);
-      
-        // if (i < originalNumTopics) {
 
-            // $("#buttons-display").append(topicBtn);
-
-        // } else {
-            
             var btnWrapper = $("<div>");
                 btnWrapper.addClass("button-wrapper");
             
@@ -104,17 +124,20 @@ function displayTopicButtons() {
 
   // /////////////////// LISTENERS
 
-  $("#add-topic-button").on("click", function(event) {
-      event.preventDefault();
+  $(document).on("click", "#add-topic-button", addButton);
+
+//   $("#add-topic-button").on("click", function(event) {
+//       event.preventDefault();
       
-      var topic = $("#gif-input").val().trim();
-      topics.push(topic);
+//       var topic = $("#gif-input").val().trim();
+//       topics.push(topic);
 
+//       displayTopicButtons(topics);
 
-      displayTopicButtons();
+//       sessionStorage.setItem("topics", JSON.stringify(topics));
 
-      $("#gif-input").val("");
-  });
+//       $("#gif-input").val("");
+//   });
 
   $(document).on("click", ".topic-button", displayGif);
 
@@ -131,5 +154,21 @@ function displayTopicButtons() {
   });
 
   // ////////////////// Initialization
-  displayTopicButtons();
+
+var topics = JSON.parse(sessionStorage.getItem("topics"));
+if (!Array.isArray(topics)) {
+    topics = initialTopics;
+    sessionStorage.setItem("topics", JSON.stringify(topics));
+  }
+displayTopicButtons(topics);
+
+// var favoriteGifs = JSON.parse(localStorage.getItem("favoriteGifs"));
+// if (Array.isArray(favoriteGifs)) {
+//     topics = initialTopics;
+//     localStorage.setItem("topics", JSON.stringify(topics));
+//   }
+//   displayTopicButtons(topics);
+
+
+//   sessionStorage.setItem("topics", JSON.stringify(initialTopics));
 
