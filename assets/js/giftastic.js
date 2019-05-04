6d4e9e33d1affd323f793e46be0b6364dd1ff62e
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-05-03 22:59:45 -0400

    add remove topic, fixed last row of gifs flexing the whole length of the row, add rating and favorite icon

--- assets/js/giftastic.js
+++ assets/js/giftastic.js
@@ -1,10 +1,27 @@
 // ///////// Variables 
 var topics = ["dog", "hippopotamus", "zebra"];
+// const originalNumTopics = topics.length;
 var searchQuanity = 10;
+
+// var topics = ["dog", "hippopotamus", "zebra", "animal", "wtf"];
+
 
 // ///////// Functions
 
-// displayGif function re-renders the HTML to display the appropriate content
+function toggleFavorite () {
+    // var gifUid = $(this).attr("data-uid");
+    console.log("favorite clicked");
+
+}
+
+function removeButton () {
+
+    var removeMe = $(this).attr("data-name");
+    topics.splice(topics.indexOf(removeMe),1);
+    displayTopicButtons();
+
+}
+
 function displayGif() {
 
     var gifName = $(this).attr("data-name");
@@ -23,29 +40,65 @@ function displayGif() {
         // console.log(response);
         for (var i=0; i<response.data.length; i++) {
 
+            // var imageUrl = response.data[i].images.fixed_height.url;
+            
+            var topicItem = $("<div>");
+            topicItem.addClass("gif-item");
+
             var imageUrl = response.data[i].images.fixed_height.url;
             var topicImage = $("<img>");
             topicImage.attr("src", imageUrl);
-            topicImage.attr("alt", gifName + " image")
-            topicImage.addClass("gif-item");
+            topicImage.attr("alt", gifName + " image");
+            topicImage.addClass("gif-image");
+            
+            var rating = response.data[i].rating;
+            var topicRating = $("<p>");
+            topicRating.text(rating);
+            topicRating.addClass("gif-rating");
 
-            topicContainer.prepend(topicImage);
+            var topicFavorite = $("<p>");
+            topicFavorite.html('<i class="fa fa-heart" aria-hidden="true"></i>');
+            topicFavorite.addClass("gif-favorite");
+
+            topicItem.append(topicImage, topicRating, topicFavorite);
+
+            topicContainer.prepend(topicItem);
         }
         topicContainer.prepend(topicTitle);
     }); //error handling?
 }
       
-function renderTopicButtons() {
+function displayTopicButtons() {
 
     $("#buttons-display").empty();
     $("#buttons-display").addClass("topic-buttons");
 
     for (var i = 0; i < topics.length; i++) {
-      var topicBtn = $("<button>");
-      topicBtn.addClass("topic-button");
-      topicBtn.attr("data-name", topics[i]);
-      topicBtn.text(topics[i]);
-      $("#buttons-display").append(topicBtn);
+     
+
+        var topicBtn = $("<button>");
+            topicBtn.addClass("topic-button");
+            topicBtn.attr("data-name", topics[i]);
+            topicBtn.text(topics[i]);
+      
+        // if (i < originalNumTopics) {
+
+            // $("#buttons-display").append(topicBtn);
+
+        // } else {
+            
+            var btnWrapper = $("<div>");
+                btnWrapper.addClass("button-wrapper");
+            
+            var removeBtn = $("<p>");
+                removeBtn.html('<i class="fa fa-times" aria-hidden="true"></i>');
+                removeBtn.addClass("remove-button");
+                removeBtn.attr("data-name", topics[i]);
+
+            btnWrapper.append(topicBtn, removeBtn);
+            $("#buttons-display").append(btnWrapper);
+        // }
+      
     }
 }
 
@@ -57,14 +110,17 @@ function renderTopicButtons() {
       var topic = $("#gif-input").val().trim();
       topics.push(topic);
 
-      // Add display favorites button (here or in render)
 
-      renderTopicButtons();
+      displayTopicButtons();
 
       $("#gif-input").val("");
   });
 
   $(document).on("click", ".topic-button", displayGif);
+
+  $(document).on("click", ".remove-button", removeButton);
+
+  $(document).on("click", ".gif-favorite", toggleFavorite);
 
   // $(document).on("click", ".favs-button", displayFav);
 
@@ -75,5 +131,5 @@ function renderTopicButtons() {
   });
 
   // ////////////////// Initialization
-  renderTopicButtons();
+  displayTopicButtons();
 
