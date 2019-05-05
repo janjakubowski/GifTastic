11c0c4f74e916e8c03c39c979ee0ec585ee07966
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-05-05 00:48:42 -0400

    displaying favorites works, added some error handling

--- assets/js/giftastic.js
+++ assets/js/giftastic.js
@@ -1,31 +1,16 @@
 // ///////// Variables 
-var initialTopics = ["dog", "hippopotamus", "zebra"];
-// const originalNumTopics = topics.length;
+var initialTopics = ["hippopotamus", "porsche", "trending"];
 var searchQuanity = 10;
 var offset = 0;
 
-// var topics = ["dog", "hippopotamus", "zebra", "animal", "wtf"];
-var topics;
-var favorites;
+var topics;         // uses sessionStorage
+var favorites;      // uses localStorage
 
 
 // ///////// Functions
 
-function displayFavorite () {
-
-    // favorites = JSON.parse(localStorage.getItem("favorites"));
-    if (favorites.length == 0) {
-        alert("You haven't selected any favorites");
-        return;
-    }
-    for (var i=0; i < favorites.length; i++) {
-        console.log()
-    }
-
-}
-
 function toggleFavorite () {
-    // var gifUid = $(this).attr("data-uid");
+
     favorites = JSON.parse(localStorage.getItem("favorites"));
 
     var uid = $(this).attr("data-uid");
@@ -48,6 +33,7 @@ function toggleFavorite () {
         $(this).attr("data-is-fav", "Y");
 
     }
+
     localStorage.setItem("favorites", JSON.stringify(favorites));
 }
 
@@ -56,6 +42,10 @@ function addButton () {
     event.preventDefault();
       
     var topic = $("#gif-input").val().trim();
+    if (topic == 'my-favorites') {
+        alert("Sorry, " + topic + "is reserved, please pick another topic to add");
+        return;
+    }
     topics.push(topic);
     displayTopicButtons(topics);
     sessionStorage.setItem("topics", JSON.stringify(topics));
@@ -74,23 +64,21 @@ function toggleGif() {
 
       var isStill = $(this).attr("data-is-still");
 
-    //   console.log("isStill: " + isStill);
-
       if (isStill === "Y") {
-        //   console.log("src: " + $(this).attr("data-animate"));
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-is-still", "N");
       } else {
-        // console.log("src: " + $(this).attr("data-still"));
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-is-still", "Y");
       }
+
 }
 
 function displayGif() {
 
     var topicName = $(this).attr("data-name");
 
+    if (topicName == 'my-favorites') {$("#gif-display").empty()};
     var allTopicContainer = $("#gif-display");
         
     var topicContainer = $("<div>");
@@ -100,28 +88,48 @@ function displayGif() {
         topicTitle.addClass("gif-item-title");
         topicTitle.text(topicName);
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search"
-                    + "?api_key=JfHIP0y01p8J0dSWEMChmhN2aWoTT7j6"
-                    + "&limit=" + searchQuanity + "&offset=" + offset + "&q=" + topicName;
+    if (topicName == 'my-favorites') {
 
-    offset += searchQuanity; 
+        if (favorites.length == 0) {
+            alert("You haven't selected any favorites");
+            return;
+        }
+        
+        uidString = JSON.parse(localStorage.getItem("favorites"));
+        var queryURL = "https://api.giphy.com/v1/gifs?"
+                + "api_key=JfHIP0y01p8J0dSWEMChmhN2aWoTT7j6" 
+                + "&ids=" + uidString.toString();
+    
+    } else {
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?"
+                + "api_key=JfHIP0y01p8J0dSWEMChmhN2aWoTT7j6"
+                + "&limit=" + searchQuanity + "&offset=" + offset + "&q=" + topicName;
+
+        offset += searchQuanity; 
+    }
     
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-
+        
         if (response.data.length == 0) {
             alert("No GIFs for " + topicName + " were found. Please make another selection.");
             return;
         }
         
+        if (response.meta.status != 200) {
+            alert("Error getting data from GIPHY API. Code = " + response.meta.status);
+            return;
+        }
+
         for (var i=0; i<response.data.length; i++) {
 
-            // var imageUrl = response.data[i].images.fixed_height_still.url;
             var stillUrl = response.data[i].images.fixed_height_still.url;
             var animateUrl = response.data[i].images.fixed_height.url;
             var rating = response.data[i].rating;
+            var uid = response.data[i].id;
             
             var topicItem = $("<div>");
                 topicItem.addClass("gif-item");
@@ -140,9 +148,14 @@ function displayGif() {
 
             var topicFavorite = $("<p>");
                 topicFavorite.html('<i class="fa fa-heart" aria-hidden="true"></i>');
+                topicFavorite.attr("data-uid", uid);
                 topicFavorite.addClass("gif-favorite");
-                topicFavorite.attr("data-uid", response.data[i].id);
-                topicFavorite.attr("data-is-fav", "N");
+                if (topicName == 'my-favorites') {
+                    topicFavorite.addClass("is-favorite");
+                    topicFavorite.attr("data-is-fav", "Y");
+                } else {
+                    topicFavorite.attr("data-is-fav", "N");
+                }   
 
             topicItem.append(topicImage, topicRating, topicFavorite);
 
@@ -150,7 +163,7 @@ function displayGif() {
         }
         topicContainer.prepend(topicTitle);
         allTopicContainer.prepend(topicContainer);
-    }); //error handling?
+    });
 }
       
 function displayTopicButtons(topics) {
@@ -160,7 +173,6 @@ function displayTopicButtons(topics) {
 
     for (var i = 0; i < topics.length; i++) {
      
-
         var topicBtn = $("<button>");
             topicBtn.addClass("topic-button");
             topicBtn.attr("data-name", topics[i]);
@@ -176,55 +188,17 @@ function displayTopicButtons(topics) {
 
             btnWrapper.append(topicBtn, removeBtn);
             $("#buttons-display").append(btnWrapper);
-        // }
       
     }
 }
 
-  // /////////////////// LISTENERS
-
-  $(document).on("click", "#add-topic-button", addButton);
-
-//   $("#add-topic-button").on("click", function(event) {
-//       event.preventDefault();
-      
-//       var topic = $("#gif-input").val().trim();
-//       topics.push(topic);
-
-//       displayTopicButtons(topics);
-
-//       sessionStorage.setItem("topics", JSON.stringify(topics));
-
-//       $("#gif-input").val("");
-//   });
-
-// $(".gif").on("click", function() {
-
-  $(document).on("click", ".topic-button", displayGif);
-
-  $(document).on("click", ".gif-image", toggleGif);
-
-  $(document).on("click", ".remove-button", removeButton);
-
-  $(document).on("click", ".my-favorites", displayFavorite);
-
-  $(document).on("click", ".gif-favorite", toggleFavorite);
-
-  // $(document).on("click", ".favs-button", displayFav);
-
-  // $(document).on("click", ".add-favorites-button", addFav);
-
-  $('input[type="radio"]').on('change', function(e) { 
-      searchQuanity = $(this).val();
-  });
-
-  // ////////////////// Initialization
+// ////////////////// Initialization
 
 topics = JSON.parse(sessionStorage.getItem("topics"));
 if (!Array.isArray(topics)) {
     topics = initialTopics;
     sessionStorage.setItem("topics", JSON.stringify(topics));
-  }
+}
 displayTopicButtons(topics);
 
 favorites = JSON.parse(localStorage.getItem("favorites"));
@@ -233,15 +207,20 @@ if (!Array.isArray(favorites)) {
     localStorage.setItem("favorites", JSON.stringify(favorites));
 }
 
+// /////////////////// LISTENERS
 
+$(document).on("click", "#add-topic-button", addButton);
 
-// var favoriteGifs = JSON.parse(localStorage.getItem("favoriteGifs"));
-// if (Array.isArray(favoriteGifs)) {
-//     topics = initialTopics;
-//     localStorage.setItem("topics", JSON.stringify(topics));
-//   }
-//   displayTopicButtons(topics);
+$(document).on("click", ".topic-button", displayGif);
 
+$(document).on("click", ".gif-image", toggleGif);
 
-//   sessionStorage.setItem("topics", JSON.stringify(initialTopics));
+$(document).on("click", ".remove-button", removeButton);
 
+$(document).on("click", ".my-favorites", displayGif);
+
+$(document).on("click", ".gif-favorite", toggleFavorite);
+
+$('input[type="radio"]').on('change', function(e) { 
+    searchQuanity = $(this).val();
+});
